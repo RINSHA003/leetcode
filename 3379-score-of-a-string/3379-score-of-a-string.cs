33fd2d42ea9b6526b12b101e625a59e4f1050ba4
RINSHA003 <136588767+RINSHA003@users.noreply.github.com>
2024-11-20 10:44:55 +0530

    Time: 0 ms (100%), Space: 38.8 MB (46.56%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int ScoreOfString(string s) {
+        int score=0;
+        for(int i=0;i<s.Length-1;i++){
+            score+=Math.Abs((int)s[i]-(int)s[i+1]);
+        }
+        return score;
+    }
+}
